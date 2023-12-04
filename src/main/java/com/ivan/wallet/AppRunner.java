1c1b97385a6c2b37c24aef8b684dc45b5c48f2d3
ssssssergeenkov <itproger181920@gmail.com>
2023-12-05 00:04:28 +0300

    added liquibase

--- src/main/java/com/ivan/wallet/AppRunner.java
+++ src/main/java/com/ivan/wallet/AppRunner.java
@@ -1,9 +1,13 @@
 package com.ivan.wallet;
 
 import com.ivan.wallet.in.WalletConsole;
+import com.ivan.wallet.liquibase.LiquibaseDemo;
 
 public class AppRunner {
     public static void main(String[] args) {
+        LiquibaseDemo liquibaseDemo = LiquibaseDemo.getInstance();
+        liquibaseDemo.runMigrations();
+
         WalletConsole walletConsole = WalletConsole.getINSTANCE();
         walletConsole.start();
     }
